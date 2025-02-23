8ab660132df9beb4da443adde52698110a274010
Deependra <deependraagarwal712@gmail.com>
2025-02-23 20:25:37 +0530

    added critic scores and cropeed images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,8 @@
 import { Game } from "@/hooks/useGames"
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react"
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react"
 import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
+import getCroppedUrl from "./services/image-url"
 
 interface Props{
     game: Game
@@ -9,12 +11,14 @@ interface Props{
 function GameCard({game}:Props){
     return (<>
         <Card.Root borderRadius={10} overflow='hidden'>
-            <Image src={game.background_image} objectFit="contain"/>
+            <Image src={getCroppedUrl(game.background_image)} objectFit="contain"/>
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
+                <HStack justifyContent={'space-between'}>
                 <PlatformIconList platforms={game.parent_platforms.map(p=>p.platform)}>
-
                 </PlatformIconList>
+                <CriticScore score={game.metacritic}></CriticScore>
+                </HStack>
             </CardBody>
         </Card.Root>
     </>)
